3dfbd897f99b8c0039a7edfcd99e1e11bfc06baa
alvarezwilfredojose <wilfredojose.alvarez@externos-cl.cencosud.com>
2025-02-04 19:50:35 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,67 @@
-import logo from './new_photo_profile_2025.jpeg';
-import './App.css';
+import Logo from "./logo.png";
+import "./App.css";
+import InstagramIcon from "./instagram-logo.png";
+import GithubIcon from "./github-logo.png";
+import LinkedinIcon from "./linkedin-logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
+        <img src={Logo} className="App-logo" alt="logo" />
         <p>
-          I am a software engineer, developing computer software for 15 years. I am proactive, responsible and committed
-          with my work.
+          I am a software engineer, developing computer software for 15 years. I
+          am proactive, responsible and committed with my work.
         </p>
-        <a
-            className="App-link"
-            href="https://www.linkedin.com/in/wilfredo-%C3%A1-a135a01b"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Linkedin
-        </a>
-        <a
-            className="App-link"
-            href="https://github.com/ingwial"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Github
-        </a>
-        <a
-            className="App-link"
-            href="https://www.instagram.com/ingwial"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Instagram
-        </a>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <img src={LinkedinIcon} alt="LinkedinIcon" height={24} />
+              </td>
+              <td>
+                <a
+                  className="App-link"
+                  href="https://www.linkedin.com/in/wilfredo-%C3%A1-a135a01b"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Linkedin
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img src={GithubIcon} alt="GithubIcon" height={24} />
+              </td>
+              <td>
+                <a
+                  className="App-link"
+                  href="https://github.com/ingwial"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img src={InstagramIcon} alt="InstagramIcon" height={24} />
+              </td>
+              <td>
+                <a
+                  className="App-link"
+                  href="https://www.instagram.com/ingwial"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Instagram
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </header>
     </div>
   );
